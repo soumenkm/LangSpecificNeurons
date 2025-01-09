3e8cc328faa55a3600aa8cd714d4f37608fb341b
Soumen Mondal <mondalsoumen00@gmail.com>
2025-01-09 21:45:15 +0530

    Change models

--- code/models.py
+++ code/models.py
@@ -248,6 +248,29 @@ class ModelForCLS(torch.nn.Module):
             loss = None
         return {"logits": out, "loss": loss}
 
+class ModelForCLM(torch.nn.Module):
+    def __init__(self, device: torch.device, model_name: str, num_class: int, quant_config: Union[BitsAndBytesConfig, None]):
+        super(ModelForCLM, self).__init__()
+        self.device = device
+        self.model_name = model_name
+        self.model_name_srt = model_name.split("/")[-1]
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+        self.base = AutoModelForCausalLM.from_pretrained(self.model_name, quantization_config=quant_config, device_map="auto")
+        self.d = self.base.config.hidden_size
+        self.c = num_class
+        self.loss_fn = torch.nn.CrossEntropyLoss()
+    
+    def forward(self, input_ids: torch.tensor, attention_mask: torch.tensor, labels: Union[None, torch.tensor]) -> torch.tensor:
+        z = self.base(input_ids=input_ids, attention_mask=attention_mask).logits # (b, T, d)
+        if labels is not None:
+            labels = labels.to(self.device)  
+            loss = self.loss_fn(z.flatten(0, 1), labels.flatten()) 
+            acc = (z.argmax(dim=-1) == labels).to(torch.float32).mean()
+        else:
+            loss = None
+            acc = None
+        return {"logits": z, "loss": loss, "acc": acc.item()}
+
 class SparseModelForCLS(torch.nn.Module):
     def __init__(self, device: torch.device, model_name: str, num_class: int, quant_config: Union[BitsAndBytesConfig, None], alpha: float):
         super(SparseModelForCLS, self).__init__()
@@ -476,6 +499,135 @@ class ModelForCLSWithLoRA(torch.nn.Module):
         else:
             prediction_output = self.model(input_ids=input_ids, attention_mask=attention_mask, labels=labels) # (b, c)
         return prediction_output
+
+class ModelForCLMWithLoRA(torch.nn.Module):
+    def __init__(self, device: torch.device, tokenizer: AutoTokenizer, model_name: str, num_class: int, lora_rank: int, lora_alpha: float, quant_config: Union[None, BitsAndBytesConfig]):
+        super(ModelForCLMWithLoRA, self).__init__()
+        self.model_name = model_name
+        self.device = device
+        self.tokenizer = tokenizer
+        self.num_class = num_class
+        self.model = ModelForCLM(device=self.device, model_name=self.model_name, num_class=self.num_class, quant_config=quant_config)
+        
+        if self.tokenizer.pad_token is None:
+            self.tokenizer.pad_token = self.tokenizer.eos_token
+            self.tokenizer.pad_token_id = self.tokenizer.eos_token_id
+        self.model.base.config.pad_token_id = self.tokenizer.pad_token_id
+        
+        for param in self.model.base.parameters():
+            param.requires_grad = False
+        
+        self.rank = lora_rank
+        self.alpha = lora_alpha
+        self.apply_lora(rank=self.rank, alpha=self.alpha)
+    
+    def apply_lora(self, rank: int, alpha: float) -> None:
+        ModelForCLMWithLoRA.replace_linear_with_lora(device=self.device, model=self.model.base, rank=rank, alpha=alpha, layer_names=['q_proj', 'k_proj', 'v_proj', 'o_proj'])            
+        # ModelForCLMWithLoRA.replace_linear_with_lora(device=self.device, model=self.model.base, rank=rank, alpha=alpha, layer_names=["lm_head"])            
+
+    @staticmethod
+    def replace_linear_with_lora(device: torch.device, model: torch.nn.Module, rank: int, alpha: float, layer_names: List[str]):
+        """layer_names = ['q_proj', 'k_proj', 'v_proj', 'o_proj']"""
+        for name, module in model.named_children():
+            if isinstance(module, torch.nn.Linear):
+                if any(proj in name for proj in layer_names):
+                    mask_A, mask_B = ModelForCLSWithLoRA.create_lora_mask(device=device, d_in=module.in_features, d_out=module.out_features, rank=rank, frozen_neuron_ids=None)
+                    linear_lora = LinearWithLoRA(module, rank, alpha, mask_A, mask_B)
+                    setattr(model, name, linear_lora) # parent is model, child is module
+            else:
+                ModelForCLMWithLoRA.replace_linear_with_lora(device, module, rank, alpha, layer_names)
+    
+    @staticmethod
+    def create_lora_mask(device: torch.device, d_in: int, d_out: int, rank: int, frozen_neuron_ids: Union[None, torch.tensor]) -> Tuple[torch.Tensor, torch.Tensor]:
+        """frozen_neuron_ids = [j | j: neuron index for a layer i]
+        """
+        mask_A = torch.ones(d_in, rank).to(device)
+        mask_B = torch.ones(rank, d_out).to(device)
+        if frozen_neuron_ids is not None:
+            for neuron_id in frozen_neuron_ids:
+                mask_B[:, neuron_id] = 0  # Zero out the entire column for the frozen neurons
+        return mask_A, mask_B
+     
+    def calc_num_params(self) -> None:
+        # Check if the requires_grad are set correctly
+        train_params = 0
+        total_params = 0
+        for name, param in self.model.named_parameters():
+            total_params += param.numel()
+            if param.requires_grad:
+                train_params += param.numel()
+        print(f"Number of total parameters: {total_params}")
+        print(f"Number of trainable parameters: {train_params}")
+        print(f"LoRA efficiency: {train_params * 100 / total_params:.3f}%")
+    
+    def get_layers(self) -> torch.nn.ModuleList:
+        m = self.model_name.lower()
+        if ("llama" in m) or ("mistral" in m) or ("sarvam" in m) or ("aya-23" in m):
+            layers = self.model.base.model.layers
+        elif "bloom" in m:
+            layers = self.model.base.model.transformer.h
+        else:
+            raise NotImplementedError("Invalid model name!")
+        return layers
+    
+    def get_target_act_module(self, layer_idx: int) -> torch.nn.Module:
+        m = self.model_name.lower()
+        mlp = self.get_layers()[layer_idx].mlp
+        if ("llama" in m) or ("mistral" in m) or ("sarvam" in m) or ("aya-23" in m):
+            target_module = mlp.act_fn
+        elif "bloom" in m:
+            target_module = mlp.gelu_impl
+        else:
+            raise NotImplementedError("Invalid model name!")
+        return target_module
+
+    def get_target_linear_module(self, layer_idx: int) -> Tuple[str, torch.nn.Linear]:
+        m = self.model_name.lower()
+        mlp = self.get_layers()[layer_idx].mlp        
+        if ("llama" in m) or ("mistral" in m) or ("sarvam" in m) or ("aya-23" in m):
+            target_module = mlp
+            name = "gate_proj"
+        elif "bloom" in m:
+            target_module = mlp
+            name = "dense_h_to_4h"
+        else:
+            raise NotImplementedError("Invalid model name!")
+        return name, target_module
+    
+    def create_hook_function(self, intervene_config: dict):
+        def hook_function(module: torch.nn.Module, inputs: torch.Tensor, outputs: torch.Tensor):
+            indices = intervene_config["indices"] # List[n x Tuple[layer_index, neuron_index]] (n neurons to intervene)
+            value = intervene_config["value"] # List[n] 
+            for i, (layer_idx, neuron_idx) in enumerate(indices):
+                if module == self.get_target_act_module(layer_idx=layer_idx):
+                    outputs.index_fill_(dim=-1, index=torch.tensor([neuron_idx]).to(self.device), value=value[i])
+        return hook_function
+
+    def register_hook(self, intervene_config: Union[dict, None]):
+        self.hooks_list = [] # List[L]
+        for layer_idx in range(len(self.get_layers())):
+            hook_function = self.create_hook_function(intervene_config=intervene_config)
+            h = self.get_target_act_module(layer_idx=layer_idx).register_forward_hook(hook_function)
+            self.hooks_list.append(h)
+    
+    def remove_hook(self):
+        for h in self.hooks_list:
+            h.remove()
+    
+    def forward(self, input_ids: torch.tensor, attention_mask: torch.tensor, intervene_config: Union[dict, None] = None, labels: Union[torch.tensor, None] = None) -> torch.tensor:
+        """intervene_config = {
+            "indices": [(i, j) | i: layer index, j: neuron index]
+            "value": [i | for each index i in indices])
+        }"""
+        assert list(input_ids.shape).__len__() == 2, "inputs rank must be 2 and inputs.shape = (b, T)"
+        if intervene_config is not None:
+            self.register_hook(intervene_config=intervene_config)
+            prediction_output = self.model(input_ids=input_ids, attention_mask=attention_mask, labels=labels) # (b, c)
+            self.remove_hook()
+        else:
+            prediction_output = self.model(input_ids=input_ids, attention_mask=attention_mask, labels=labels) # (b, c)
+        return prediction_output
+  
         
 def main_lora(model_name: str, device: torch.device) -> None:
     tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -525,11 +677,30 @@ def main_mlm(model_name: str, device: torch.device) -> None:
         out2 = model(input_ids=input_dict["input_ids"], attention_mask=input_dict["attention_mask"], intervene_config=intervene_config, labels=input_dict["labels"]) # (b, c)
     print(out1["logits"].sum(), out1["loss"]) 
     print(out2["logits"].sum(), out2["loss"])  
-    
+
+def main_clm(model_name: str, device: torch.device) -> None:
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    quant_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_quant_type='nf4',  
+            bnb_4bit_compute_dtype=torch.bfloat16,  
+            bnb_4bit_use_double_quant=True,  
+    )
+    model = ModelForCLMWithLoRA(device=device, tokenizer=tokenizer, model_name=model_name, num_class=3, lora_rank=8, lora_alpha=16, quant_config=quant_config).to(device)
+    input_ids = torch.randint(low=0, high=100, size=(16, 256)).to(device) # (b, T)
+    labels = torch.randint(low=0, high=3, size=(16, 256)).to(device) # (b,)
+    attention_mask = torch.ones(size=(16, 256)).to(device) # (b, T)
+    print(model)
+    model.calc_num_params()
+    model.train()
+    with torch.autocast("cuda"):
+        out = model(input_ids=input_ids, attention_mask=attention_mask, intervene_config=None, labels=labels) # (b, c)
+    print(out["logits"].sum(), out["loss"]) 
+ 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using {device}...")
     
     # main_mlm(models_map["llama3"], device=device)
-    main_lora("meta-llama/Meta-Llama-3.1-8B", device=device)
+    main_clm("meta-llama/Meta-Llama-3.1-8B", device=device)
     
