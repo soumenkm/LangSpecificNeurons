d78f0bf31605038e72aee378af89d6c846806da4
Soumen Mondal <mondalsoumen00@gmail.com>
2025-01-28 16:27:52 +0530

    Add results

--- code/lora_instruct_finetune.py
+++ code/lora_instruct_finetune.py
@@ -1,7 +1,7 @@
 import os, json, pickle, torch, wandb
 if __name__ == "__main__":
     wandb.login()
-    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "2"
     
 torch.manual_seed(42)
 from pathlib import Path
@@ -201,12 +201,12 @@ class LoRAFineTuner:
 def main(model_name: str, device: torch.device) -> None:
     config = {
         "model_name": model_name, "task_name": "XQUAD-FT",
-        "method": "lape/set1", "lang": "en", "finetune_lang": "null", # ["en", "vi", "en+vi", "null", "set1_en"]
+        "method": "lape/set6", "lang": "en", "finetune_lang": "null", # ["en", "vi", "en+vi", "null", "set1_en"]
         "num_epochs": 10, "num_steps": None, "batch_size": 4, "max_context_length": 512, # steps are auto calculated
         "train_frac": 1.0, "eval_frac": 0.1,
         "initial_lr": 5e-5, "lora_rank": 64, "lora_alpha": 128, "max_grad_norm": 10.0, "weight_decay": 0.1,
         "adam_betas": (0.95, 0.999), "grad_acc_steps": 1, "num_ckpt_per_epoch": 1, "is_4bit_quant": True, "fp16": False, "bf16": True,
-        "wandb_log": True
+        "wandb_log": False
     }
     trainer = LoRAFineTuner(device=device, config=config)
     trainer.train()
