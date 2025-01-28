29a95fc3f2da99d82fd8fd52e6713b6e7b259591
Soumen Mondal <mondalsoumen00@gmail.com>
2025-01-28 10:25:53 +0530

    Add activation for mistral

--- code/activation.py
+++ code/activation.py
@@ -273,8 +273,8 @@ def main(model_name: str, device: torch.device) -> None:
             bnb_4bit_compute_dtype=torch.bfloat16,  
             bnb_4bit_use_double_quant=True,  
     )
-    for lang in ["bn"]: 
-        for method in ["act_stat"]:
+    for lang in ["te", "mr"]: 
+        for method in ["all_act"]:
             rel = NeuronRelevance(device=device, model_name=model_name, quant_config=quant_config, lang=lang, scoring_method=method)
             if method == "act_stat":
                 out = rel.get_act_stat_data(batch_size=4, data_frac=0.1)
